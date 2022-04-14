acc644d2dc4917ef91360eb9b4de447b1b6e53b7
Josh Haver <jdhav2@gmail.com>
2022-04-14 11:29:20 -0400

    Wrote survey model

--- itsmeapp/models.py
+++ itsmeapp/models.py
@@ -4,4 +4,17 @@ from django.db import models
 
 
 class Survey(models.Model):
-    text = models.TextField()
+    gender = models.TextField()
+    ethnicity = models.TextField()
+    grade = models.TextField()
+    major = models.TextField()
+    discussion = models.TextField()
+    gpa = models.TextField()
+    program = models.TextField()
+    professional = models.TextField()
+    enrollment = models.TextField()
+    prior = models.TextField()
+    internship = models.BooleanField()
+    research = models.BooleanField()
+    parent_engineer = models.BooleanField()
+    family_engineer = models.BooleanField()
