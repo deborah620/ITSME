fc029a7b4608e3ec9029d5602a902e0ea62c33f5
Josh Haver <jdhav2@gmail.com>
2022-04-14 14:03:52 -0400

    Post request persists data to sqlite db

--- itsmeapp/views.py
+++ itsmeapp/views.py
@@ -5,23 +5,34 @@ from rest_framework import status
 from django.http import HttpResponse
 from .models import Survey
 
+
 # Create your views here.
 def index(request):
     return render(request, 'itsmeapp/index.html', context={})
 
 
+class SurveyResultsAPI(APIView):
 
-class BasicAPI(APIView):
-    def get(self, request):
-        data = {
-            'first_name': 'grant',
-            'last_name': 'zhu'
-        }
-        return Response(data)
-
-    # HERE IS THE POST API
+    # post api for survey results
+    # request object has info about request, ie POST, data, etc
     def post(self, request):
         req_data = request.data
 
-        survey_data = Survey(gender=req_data.get('gender'))
+        survey_data = Survey(
+            gender=req_data.get('gender'),
+            ethnicity=req_data.get('ethnicity'),
+            grade=req_data.get('grade'),
+            major=req_data.get('major'),
+            discussion=req_data.get('discussion'),
+            gpa=req_data.get('gpa'),
+            program=req_data.get('program'),
+            professional=req_data.get('professional'),
+            enrollment=req_data.get('enrollment'),
+            prior=req_data.get('prior'),
+            internship=req_data.get('internship'),
+            research=req_data.get('research'),
+            parent_engineer=req_data.get('parent-engineer'),
+            family_engineer=req_data.get('family-engineer')
+        )
+        survey_data.save()
         return Response(req_data, status=status.HTTP_200_OK)
