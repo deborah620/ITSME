1d66fae896d3046716b7c4a033108d31cff72a4b
Josh Haver <jdhav2@gmail.com>
2022-04-14 13:05:01 -0400

    Added Post api to url

--- itsmeapp/urls.py
+++ itsmeapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import BasicAPI
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', BasicAPI.as_view())
 ]
