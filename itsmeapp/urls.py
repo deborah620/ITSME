89585c95dd549c5dc7beaffde160f21d740dc077
deborah620 <d169@umbc.edu>
2022-04-27 18:09:25 -0400

    added action to form

--- itsmeapp/urls.py
+++ itsmeapp/urls.py
@@ -5,5 +5,5 @@ from .views import SurveyResultsAPI
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api/', SurveyResultsAPI.as_view()),
+    path('api/', SurveyResultsAPI.as_view(), name='survey_result_api')
 ]
