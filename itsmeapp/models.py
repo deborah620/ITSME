681ee313ab3b6de1f42a138c5c330944a5061f5b
jep2032 <jep2032@gmail.com>
2022-04-28 21:37:15 -0400

    Added model for survey results

--- itsmeapp/models.py
+++ itsmeapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Survey(models.Model):
-    # models for the html front end survey results
+    # models for the html front end survey inputs
     gender = models.TextField()
     ethnicity = models.TextField()
     grade = models.TextField()
@@ -19,3 +19,24 @@ class Survey(models.Model):
     research = models.BooleanField()
     parent_engineer = models.BooleanField()
     family_engineer = models.BooleanField()
+
+class Results(models.Model):
+    # models for the html front end survey results
+    score_1 = models.TextField()
+    score_2 = models.TextField()
+    score_3 = models.TextField()
+    score_4 = models.TextField()
+    score_5 = models.TextField()
+    score_6 = models.TextField()
+    score_7 = models.TextField()
+    score_8 = models.TextField()
+    score_9 = models.TextField()
+    score_10 = models.TextField()
+    score_11 = models.TextField()
+    score_12 = models.TextField()
+    score_13 = models.TextField()
+    score_14 = models.TextField()
+    score_15 = models.TextField()
+    eps_total = models.TextField()
+    spve = models.TextField()
+    me = models.TextField()
