050e8406689494277d32ef090e92ac3724ff2774
deborah620 <d169@umbc.edu>
2022-05-09 15:03:05 -0400

    updated views.py

--- itsmeapp/models.py
+++ itsmeapp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Survey(models.Model):
     # models for the html front end survey results
-    # objects = models.Manager()
+    objects = models.Manager()
     gender = models.TextField()
     ethnicity = models.TextField()
     grade = models.TextField()
